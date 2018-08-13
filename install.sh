9f5ac43e97f38f7ea468f92c6f26ea406652ef66
Tyler Scott <tyler@inversoft.com>
2018-08-13 12:04:44 -0600

    Added the ability to overwrite TARGET_DIR, made curl not silent so the user sees progress, added symlinking after unzipping.

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-TARGET_DIR=${HOME}/fusionauth
+# Default to home/fusionAuth but keep any existing value.
+TARGET_DIR=${TARGET_DIR:-${HOME}/fusionAuth}
 VERSION=$(curl https://www.inversoft.com/latest-passport-version)
 SUFFIX=""
 
 install_linux() {
   # Detect dpkg and rpm, fallback to zip
-  if hash dpkg; then
+  if hash dpkg > /dev/null 2>&1; then
     install_deb;
-  elif hash rpm; then
+  elif hash rpm > /dev/null 2>&1; then
     install_rpm;
   else
     install_zip;
@@ -16,27 +17,36 @@ install_linux() {
 }
 
 install_deb() {
-  curl -fsSL -o /tmp/fusionAuth-backend.deb "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend_${VERSION}-1_all.deb"
-  curl -fsSL -o /tmp/fusionAuth-search-engine.deb "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine_${VERSION}-1_all.deb"
+  echo "Installing deb packages"
+
+  curl -fSL -o /tmp/fusionAuth-backend.deb "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend_${VERSION}-1_all.deb"
+  curl -fSL -o /tmp/fusionAuth-search-engine.deb "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine_${VERSION}-1_all.deb"
 
   sudo dpkg -i /tmp/fusionAuth-backend.deb /tmp/fusionAuth-search-engine.deb
 }
 
 install_rpm() {
-  curl -fsSL -o /tmp/fusionAuth-backend.rpm "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend-${VERSION}-1.noarch.rpm"
-  curl -fsSL -o /tmp/fusionAuth-search-engine.rpm "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine-${VERSION}-1.noarch.rpm"
+  echo "Installing rpm packages"
+
+  curl -fSL -o /tmp/fusionAuth-backend.rpm "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend-${VERSION}-1.noarch.rpm"
+  curl -fSL -o /tmp/fusionAuth-search-engine.rpm "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine-${VERSION}-1.noarch.rpm"
 
   sudo rpm -i /tmp/fusionAuth-backend.rpm /tmp/fusionAuth-search-engine.rpm
 }
 
 install_zip() {
-  curl -fsSL -o /tmp/fusionAuth-backend.zip "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend-${SUFFIX}${VERSION}.zip"
-  curl -fsSL -o /tmp/fusionAuth-search-engine.zip "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine-${SUFFIX}${VERSION}.zip"
+  echo "Installing zip packages to ${TARGET_DIR}"
 
-  sudo mkdir -p ${TARGET_DIR}
+  curl -fSL -o /tmp/fusionAuth-backend.zip "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-backend-${SUFFIX}${VERSION}.zip"
+  curl -fSL -o /tmp/fusionAuth-search-engine.zip "https://storage.googleapis.com/inversoft_products_j098230498/products/passport/${VERSION}/passport-search-engine-${SUFFIX}${VERSION}.zip"
 
-  unzip /tmp/fusionAuth-backend.zip -n -d ${TARGET_DIR}
-  unzip /tmp/fusionAuth-search-engine.zip -n -d ${TARGET_DIR}
+  mkdir -p ${TARGET_DIR}
+
+  unzip -n /tmp/fusionAuth-backend.zip -d ${TARGET_DIR}
+  unzip -n /tmp/fusionAuth-search-engine.zip -d ${TARGET_DIR}
+
+  ln -sf ${TARGET_DIR}/passport-backend-${VERSION} ${TARGET_DIR}/fusionAuth-backend
+  ln -sf ${TARGET_DIR}/passport-search-engine-${VERSION} ${TARGET_DIR}/fusionAuth-search-engine
 }
 
 case $(uname -s) in
